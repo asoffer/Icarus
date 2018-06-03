49104f07bb7e855199f9992bf602948945150983
Andy Soffer <andy.soffer@gmail.com>
2018-06-02 23:33:46 -0400

    Checking interfaces for variants, but not emitting code properly.

--- src/ir/interface.cc
+++ src/ir/interface.cc
@@ -6,23 +6,34 @@ namespace IR {
 std::vector<std::string> Interface::MatchErrors(const type::Type* t) const {
   std::vector<std::string> errors;
   if (!field_map_.empty()) {
-    if (!t->is<type::Struct>()) {
-      errors.push_back(t->to_string() +
-                       " is not a struct and therefore has no named members.");
-      return errors;
-    }
+    auto CheckStruct =
+        [&errors, this](const type::Type* t) {
+          if (!t->is<type::Struct>()) {
+            errors.push_back(
+                t->to_string() +
+                " is not a struct and therefore has no named members.");
+            return;
+          }
+          const auto* s = &t->as<type::Struct>();
+          for (const auto & [ name, type ] : field_map_) {
+            if (auto* field = s->field(name)) {
+              if (field->type != type) {
+                errors.push_back(
+                    s->to_string() + " has a meber named " + name +
+                    ", but it has type " + field->type->to_string() +
+                    ", rather than the required " + type->to_string());
+              }
+            } else {
+              errors.push_back(s->to_string() + " has no member named " + name +
+                               ".");
+            }
+          }
+        };
 
-    const auto* s = &t->as<type::Struct>();
-    for (const auto & [ name, type ] : field_map_) {
-      if (auto* field = s->field(name)) {
-        if (field->type != type) {
-          errors.push_back(s->to_string() + " has a meber named " + name +
-                           ", but it has type " + field->type->to_string() +
-                           ", rather than the required " + type->to_string());
-        }
-      } else {
-        errors.push_back(s->to_string() + " has no member named " + name + ".");
-      }
+    if (t->is<type::Variant>()) {
+      for (auto* v : t->as<type::Variant>().variants_) { CheckStruct(v); }
+    } else {
+      CheckStruct(t);
     }
   }
 
