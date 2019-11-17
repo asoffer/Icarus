d4eb925f045e07dc462aa68025104a2499510fc3
asoffer <andy.soffer@gmail.com>
2019-11-17 18:14:00 -0500

    variant tests

--- type/variant_test.cc
+++ type/variant_test.cc
@@ -10,7 +10,8 @@ namespace {
 TEST_CASE("Var") {
   SECTION("empty") {
     CHECK(Var({}) == Void());
-    CHECK(Var({}) == Void());  // Second time because of caching.
+    // Second time because of caching.
+    CHECK(Var({}) == Void());
     Type const* v = Var({});
     CHECK(Var({}) == v);
   }
@@ -18,12 +19,16 @@ TEST_CASE("Var") {
   SECTION("one") {
     CHECK(Var({Int64}) != Void());
     CHECK(Var({Int64}) == Var({Int64}));
+    CHECK(Var({Int64}) ==  Int64);
+    // Second time because of caching.
     CHECK(Var({Int64}) == Int64);
     CHECK(Var({Int64, Int64}) == Int64);
   }
 
   SECTION("many") {
     CHECK(Var({Int64, Bool}) != Void());
+    CHECK(Var({Int64, Bool}) == Var({Bool, Int64}));
+    // Second time because of caching.
     CHECK(Var({Int64, Bool}) == Var({Bool, Int64}));
     CHECK(Var({Bool, Int64, Bool}) == Var({Bool, Int64}));
   }
@@ -42,6 +47,13 @@ TEST_CASE("MultiVar") {
 
   CHECK(MultiVar({std::vector{Int64, Bool}, std::vector{Int64, Int64}}) ==
         Tup({Int64, Var({Bool, Int64})}));
+}
+
+TEST_CASE("contains") {
+  Variant const& var = Var({Int32, Int64})->as<Variant>();
+  CHECK_FALSE(var.contains(Bool));
+  CHECK(var.contains(Int32));
+  CHECK_FALSE(var.contains(&var));
 }
 
 }  // namespace
