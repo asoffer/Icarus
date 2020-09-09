7feca7d7aba95c07303af549abab953d123a7ee8
asoffer <andy.soffer@gmail.com>
2020-09-09 08:31:23 -0400

    Forgot to add these in last commit.

---
+++ compiler/emit/array_literal.cc
@@ -0,0 +1,51 @@
+#include "ast/ast.h"
+#include "compiler/compiler.h"
+
+namespace compiler {
+
+ir::Value Compiler::EmitValue(ast::ArrayLiteral const *node) {
+  auto *t          = ASSERT_NOT_NULL(data().qual_type(node))->type();
+  auto alloc       = builder().TmpAlloca(t);
+  auto typed_alloc = type::Typed<ir::RegOr<ir::Addr>>(
+      ir::RegOr<ir::Addr>(alloc), type::Ptr(t));
+  EmitMoveInit(node, absl::MakeConstSpan(&typed_alloc, 1));
+  return ir::Value(alloc);
+}
+
+void Compiler::EmitCopyInit(
+    ast::ArrayLiteral const *node,
+    absl::Span<type::Typed<ir::RegOr<ir::Addr>> const> to_vars) {
+  ASSERT(to_vars.size() == 1u);
+  type::Array const &array_type =
+      ASSERT_NOT_NULL(data().qual_type(node))->type()->as<type::Array>();
+  auto const *data_type_ptr = type::Ptr(array_type.data_type());
+  auto elem = builder().Index(type::Ptr(&array_type), to_vars[0]->reg(), 0);
+  // Skip the last entry so we don't increment past the end of the array.
+  for (size_t i = 0; i + 1 < array_type.length(); ++i) {
+    type::Typed<ir::RegOr<ir::Addr>> to_var(elem, data_type_ptr);
+    EmitCopyInit(node->elems()[i], absl::MakeConstSpan(&to_var, 1));
+    elem = builder().PtrIncr(elem, 1, data_type_ptr);
+  }
+  type::Typed<ir::RegOr<ir::Addr>> to_var(elem, data_type_ptr);
+  EmitCopyInit(node->elems().back(), absl::MakeConstSpan(&to_var, 1));
+}
+
+void Compiler::EmitMoveInit(
+    ast::ArrayLiteral const *node,
+    absl::Span<type::Typed<ir::RegOr<ir::Addr>> const> to_vars) {
+  ASSERT(to_vars.size() == 1u);
+  type::Array const &array_type =
+      ASSERT_NOT_NULL(data().qual_type(node))->type()->as<type::Array>();
+  auto *data_type_ptr = type::Ptr(array_type.data_type());
+  auto elem = builder().Index(type::Ptr(&array_type), to_vars[0]->reg(), 0);
+  // Skip the last entry so we don't increment past the end of the array.
+  for (size_t i = 0; i + 1 < array_type.length(); ++i) {
+    type::Typed<ir::RegOr<ir::Addr>> to_var(elem, data_type_ptr);
+    EmitMoveInit(node->elems()[i], absl::MakeConstSpan(&to_var, 1));
+    elem = builder().PtrIncr(elem, 1, data_type_ptr);
+  }
+  type::Typed<ir::RegOr<ir::Addr>> to_var(elem, data_type_ptr);
+  EmitMoveInit(node->elems().back(), absl::MakeConstSpan(&to_var, 1));
+}
+
+}  // namespace compiler
