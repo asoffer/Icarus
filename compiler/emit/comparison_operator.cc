6aab3a2b2a9c9b107ddc39eecff7e67fc380f88b
asoffer <andy.soffer@gmail.com>
2020-10-27 08:58:33 -0400

    Comparison operator tests.

---
+++ compiler/emit/comparison_operator.cc
@@ -0,0 +1,122 @@
+#include "ast/ast.h"
+#include "compiler/compiler.h"
+
+namespace compiler {
+namespace {
+ir::RegOr<bool> EmitPair(Compiler &compiler,
+                         ast::ComparisonOperator const *node, size_t index,
+                         type::Typed<ir::Value> lhs,
+                         type::Typed<ir::Value> rhs) {
+  auto &bldr = compiler.builder();
+  auto op    = node->ops()[index];
+
+  if (lhs.type()->is<type::Array>() and rhs.type()->is<type::Array>()) {
+    NOT_YET();
+  } else if (lhs.type()->is<type::Struct>() or rhs.type()->is<type::Struct>()) {
+    NOT_YET();
+
+  } else {
+    switch (op) {
+      case frontend::Operator::Lt:
+        return type::ApplyTypes<int8_t, int16_t, int32_t, int64_t, uint8_t,
+                                uint16_t, uint32_t, uint64_t, float, double,
+                                ir::FlagsVal>(lhs.type(), [&]<typename T>() {
+          return bldr.Lt(lhs->get<ir::RegOr<T>>(), rhs->get<ir::RegOr<T>>());
+        });
+      case frontend::Operator::Le:
+        return type::ApplyTypes<int8_t, int16_t, int32_t, int64_t, uint8_t,
+                                uint16_t, uint32_t, uint64_t, float, double,
+                                ir::FlagsVal>(lhs.type(), [&]<typename T>() {
+          return bldr.Le(lhs->get<ir::RegOr<T>>(), rhs->get<ir::RegOr<T>>());
+        });
+      case frontend::Operator::Eq:
+        if (lhs.type() == type::Block) {
+          auto val1 = lhs->get<ir::RegOr<ir::Block>>();
+          auto val2 = rhs->get<ir::RegOr<ir::Block>>();
+          if (not val1.is_reg() and not val2.is_reg()) {
+            return val1.value() == val2.value();
+          }
+        }
+        return type::ApplyTypes<bool, int8_t, int16_t, int32_t, int64_t,
+                                uint8_t, uint16_t, uint32_t, uint64_t, float,
+                                double, type::Type, ir::EnumVal, ir::FlagsVal,
+                                ir::Addr>(lhs.type(), [&]<typename T>() {
+          return bldr.Eq(lhs->get<ir::RegOr<T>>(), rhs->get<ir::RegOr<T>>());
+        });
+      case frontend::Operator::Ne:
+        if (lhs.type() == type::Block) {
+          auto val1 = lhs->get<ir::RegOr<ir::Block>>();
+          auto val2 = rhs->get<ir::RegOr<ir::Block>>();
+          if (not val1.is_reg() and not val2.is_reg()) {
+            return val1.value() == val2.value();
+          }
+        }
+        return type::ApplyTypes<bool, int8_t, int16_t, int32_t, int64_t,
+                                uint8_t, uint16_t, uint32_t, uint64_t, float,
+                                double, type::Type, ir::EnumVal, ir::FlagsVal,
+                                ir::Addr>(lhs.type(), [&]<typename T>() {
+          return bldr.Ne(lhs->get<ir::RegOr<T>>(), rhs->get<ir::RegOr<T>>());
+        });
+      case frontend::Operator::Ge:
+        return type::ApplyTypes<int8_t, int16_t, int32_t, int64_t, uint8_t,
+                                uint16_t, uint32_t, uint64_t, float, double,
+                                ir::FlagsVal>(lhs.type(), [&]<typename T>() {
+          return bldr.Ge(lhs->get<ir::RegOr<T>>(), rhs->get<ir::RegOr<T>>());
+        });
+      case frontend::Operator::Gt:
+        return type::ApplyTypes<int8_t, int16_t, int32_t, int64_t, uint8_t,
+                                uint16_t, uint32_t, uint64_t, float, double,
+                                ir::FlagsVal>(lhs.type(), [&]<typename T>() {
+          return bldr.Gt(lhs->get<ir::RegOr<T>>(), rhs->get<ir::RegOr<T>>());
+        });
+        // TODO case frontend::Operator::And: cmp = *lhs; break;
+      default: UNREACHABLE();
+    }
+  }
+}
+
+type::Typed<ir::Value> EmitTypedValue(Compiler &c,
+                                      ast::Expression const *expr) {
+  return type::Typed<ir::Value>(c.EmitValue(expr),
+                                c.context().qual_type(expr)->type());
+}
+
+}  // namespace
+
+ir::Value Compiler::EmitValue(ast::ComparisonOperator const *node) {
+  if (node->ops().size() == 1) {
+    return ir::Value(EmitPair(*this, node, 0,
+                              EmitTypedValue(*this, node->exprs()[0]),
+                              EmitTypedValue(*this, node->exprs()[1])));
+  }
+
+  std::vector<ir::BasicBlock const *> phi_blocks;
+  std::vector<ir::RegOr<bool>> phi_values;
+  auto lhs         = EmitTypedValue(*this, node->exprs().front());
+  auto *land_block = builder().AddBlock();
+  for (size_t i = 0; i + 1 < node->ops().size(); ++i) {
+    auto rhs = EmitTypedValue(*this, node->exprs()[i + 1]);
+    auto cmp = EmitPair(*this, node, i, lhs, rhs);
+
+    phi_blocks.push_back(builder().CurrentBlock());
+    phi_values.push_back(false);
+    auto *next_block = builder().AddBlock();
+    builder().CondJump(cmp, next_block, land_block);
+    builder().CurrentBlock() = next_block;
+    lhs                      = std::move(rhs);
+  }
+
+  // Once more for the last element, but don't do a conditional jump.
+  auto rhs = EmitTypedValue(*this, node->exprs().back());
+  phi_blocks.push_back(builder().CurrentBlock());
+  phi_values.push_back(
+      EmitPair(*this, node, node->exprs().size() - 2, lhs, rhs));
+  builder().UncondJump(land_block);
+
+  builder().CurrentBlock() = land_block;
+
+  return ir::Value(
+      builder().Phi<bool>(std::move(phi_blocks), std::move(phi_values)));
+}
+
+}  // namespace compiler
---
+++ compiler/emit/comparison_operator_test.cc
@@ -0,0 +1,101 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "test/module.h"
+#include "type/primitive.h"
+
+namespace compiler {
+namespace {
+
+struct TestCase {
+  std::string expr;
+  ir::Value expected;
+};
+
+using ComparisonOperatorTest = testing::TestWithParam<TestCase>;
+TEST_P(ComparisonOperatorTest, Access) {
+  auto const &[expr, expected] = GetParam();
+  test::TestModule mod;
+  // TODO: We can't use `s` as the field member because the compiler thinks
+  // there's an ambiguity (there isn't).
+  mod.AppendCode(R"(
+  S ::= struct {
+    n: int64
+    p: *int64
+    sp: *S
+  }
+  )");
+  auto const *e  = mod.Append<ast::Expression>(expr);
+  auto const *qt = mod.context().qual_type(e);
+  ASSERT_NE(qt, nullptr);
+  auto t = qt->type();
+  ASSERT_TRUE(t.valid());
+  auto result =
+      mod.compiler.Evaluate(type::Typed<ast::Expression const *>(e, t));
+  ASSERT_TRUE(result);
+  EXPECT_EQ(*result, expected);
+}
+
+// Note: We test both with literals and with a unary-operator applied directly
+// to a function call. The former helps cover the constant-folding mechanisms
+// built in to the ir::Builder. The latter helps cover the common case for code
+// emission.
+INSTANTIATE_TEST_SUITE_P(
+    All, ComparisonOperatorTest,
+    testing::ValuesIn({
+        TestCase{.expr     = R"(((x: int64, y: int64) => x < y)(1, 2))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x < y)(1, 1))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x < y)(2, 1))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x <= y)(1, 2))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x <= y)(1, 1))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x <= y)(2, 1))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x == y)(1, 2))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x == y)(1, 1))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x != y)(2, 1))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x != y)(1, 1))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x > y)(1, 2))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x > y)(1, 1))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x > y)(2, 1))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x >= y)(1, 2))",
+                 .expected = ir::Value(false)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x >= y)(1, 1))",
+                 .expected = ir::Value(true)},
+        TestCase{.expr     = R"(((x: int64, y: int64) => x >= y)(2, 1))",
+                 .expected = ir::Value(true)},
+
+        TestCase{
+            .expr = R"(((x: int64, y: int64, z: int64) => x < y < z)(1, 2, 3))",
+            .expected = ir::Value(true)},
+        TestCase{
+            .expr = R"(((x: int64, y: int64, z: int64) => x < y < z)(1, 2, 1))",
+            .expected = ir::Value(false)},
+        TestCase{
+            .expr = R"(((x: int64, y: int64, z: int64) => x < y <= z)(1, 2, 2))",
+            .expected = ir::Value(true)},
+        TestCase{
+            .expr = R"(((x: int64, y: int64, z: int64) => x < y > z)(2, 3, 1))",
+            .expected = ir::Value(true)},
+        TestCase{
+            .expr = R"(((x: int64, y: int64, z: int64) => x < y > z)(1, 2, 1))",
+            .expected = ir::Value(true)},
+        TestCase{
+            .expr = R"(((x: int64, y: int64, z: int64) => x < y > z)(2, 3, 4))",
+            .expected = ir::Value(false)},
+    }));
+
+// TODO: Add a test that covers pointer parameters.
+
+}  // namespace
+}  // namespace compiler
