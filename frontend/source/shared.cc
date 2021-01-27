984b7ee2647930fcc0b088726a76de44db7f28cd
CJ Carey <perimosocordiae@gmail.com>
2021-01-27 00:42:01 -0500

    Unbreak diagnostic printing from SharedSourceType

--- frontend/source/shared.cc
+++ frontend/source/shared.cc
@@ -7,7 +7,7 @@ namespace {
 struct SharedSourceType : Source {
   SourceChunk ReadUntil(char delim) override { UNREACHABLE(); }
   std::string_view line(LineNum line_num) const override { UNREACHABLE(); }
-  std::string FileName() const override { UNREACHABLE(); }
+  std::string FileName() const override { return ""; }
   SourceBuffer& buffer() override { UNREACHABLE(); }
   SourceBuffer const& buffer() const override { UNREACHABLE(); }
 };
