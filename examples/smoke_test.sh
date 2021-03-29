f749b6b344422791e71254cbc578fd920418e7ce
CJ Carey <perimosocordiae@gmail.com>
2021-03-29 15:25:52 -0400

    Turns out the culprit wasn't primes.ic

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -15,5 +15,4 @@ $binary "$examples/fibonacci.ic" >/dev/null && echo fibonacci OK
 $binary "$examples/fizzbuzz.ic" >/dev/null && echo fizzbuzz OK
 $binary "$examples/function_calls.ic" >/dev/null && echo function_calls OK
 $binary "$examples/ping_pong.ic" | head >/dev/null && echo ping_pong OK
-# hangs forever on GitHub Actions
-#$binary "$examples/primes.ic" >/dev/null && echo primes OK
+$binary "$examples/primes.ic" >/dev/null && echo primes OK
