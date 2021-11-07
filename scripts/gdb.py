4d34c009aaf195c6db5c181913c59e7da10a4931
asoffer <andy.soffer@gmail.com>
2021-11-06 22:33:44 -0400

    Putting useful tools in the scripts directory.

---
+++ scripts/gdb.py
@@ -0,0 +1,54 @@
+"""
+Python script for eliding frames from GDB backtraces that do not provide
+valuable information.
+
+This script elides any ast- or type-visitor frames, C++ standard library
+internals and some frames surrounding std::function's call operator.
+
+Enable this script by adding the following line to yout `.gdbinit` file:
+
+```
+source ~/.gdb/icarus.py
+```
+
+"""
+
+import gdb
+import re
+
+def should_display(f):
+    if type(f.function()) == int:
+        # Integers represent "non-virtual thunks", which are typically offsets
+        # for vtables of types with multiple inheritance.
+        return False
+
+    for pattern in [
+            r"^compiler::Compiler::Visit",
+            r"^compiler::Compiler::VerifyType\(ast::Node const",
+            r"^compiler::Compiler::EmitToBuffer\(ast::Node const",
+            r"^ast::Visitor<",
+            r"^type::Visitor<",
+            r"^ast::[^:]*::Accept",
+            r"^type::[^:]*::Accept",
+            r"^std::__invoke",
+            r"^std::_[A-Z]",
+            r"^std::function",
+            r"^std::invoke",
+        ]:
+        if re.match(pattern, f.function()) is not None:
+            return False
+    return True
+
+
+class IcarusFilter:
+    def __init__(self):
+        self.name = 'IcarusFilter'
+        self.enabled = True
+        self.priority = 0
+
+        gdb.current_progspace().frame_filters[self.name] = self
+
+    def filter(self, frame_iter):
+        return filter(should_display, frame_iter)
+
+IcarusFilter()
---
+++ scripts/syntax.vim
@@ -0,0 +1,33 @@
+" Basic syntax highlighting for Icarus in Vim.
+" You can enable this by default for files with the `.ic` extension by putting
+" the following in your .vimrc, and placing this file in a location that vim
+" will find it on startup (such as the directory `~/.vim/syntax/`).
+"
+" ```
+" au BufNewFile,BufRead *.ic set filetype=icarus
+" ```
+
+if exists("b:current_syntax")
+  finish
+endif
+
+syntax match Operator ":*=*"
+syntax match Operator "[,'$]"
+syntax match Operator "[~`+*\-/%<>&|!]=\?"
+syntax match Identifier "#[a-zA-Z_=0-9()"]\+"
+syntax match Identifier "#{[a-zA-Z_=0-9 ()"]\+}"
+syntax match Identifier "#\.[a-zA-Z_0-9]\+"
+syntax region MultiLineComment start=/\/\*/ skip=/\\./ end=/\*\// contains=MultiLineComment
+hi link MultiLineComment Comment
+
+syntax keyword Keyword move copy import flags enum struct return jump scope as goto foreign bytes align opaque block init destroy and or xor not abort interface slice callable reserve_memory
+syntax keyword Boolean    true false
+syntax keyword Constant   bool i8 i16 i32 i64 u8 u16 u32 u64 f32 f64 type module null char byte
+syntax match   Character  "![^\\]\|!\\[abfnrtv0]"
+syntax match   Number     "\([_a-zA-Z]\)\@<!\d\+\.\?\d*"
+syntax match   Comment    "\/\/.*$"
+syntax region  String     start=/\v"/ skip=/\v\\./ end=/\v"/
+syntax match   Function   "[a-zA-Z_0-9]\+\((\)\@="
+syntax match   Function   "\('\)\@<=[a-zA-Z_0-9]\+"
+
+let b:current_syntax = "icarus"
