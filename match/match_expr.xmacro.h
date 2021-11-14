b956b5d9428f2540a861b19f98810b089ec4f9be
asoffer <andy.soffer@gmail.com>
2021-11-14 09:45:58 -0500

    Deleting format/ and match/ which have been placeholders for years.

--- match/match_expr.xmacro.h
+++
@@ -1,3 +0,0 @@
-ICARUS_AST_VISITOR(void match_expr(match::Match *visitor,
-                                   match::Match::State *state) const,
-                   { visitor->MatchExpr(this, state); });
