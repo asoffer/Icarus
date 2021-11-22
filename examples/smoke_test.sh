97ae98c7741b703281c21ea559360b04d0018964
CJ Carey <perimosocordiae@gmail.com>
2021-11-22 11:21:31 -0500

    Make the ping_pong example terminate and test it

--- examples/smoke_test.sh
+++ examples/smoke_test.sh
@@ -9,6 +9,7 @@ root=$(dirname "$examples")
 binary="$root/bazel-bin/compiler/interpret"
 export ICARUS_MODULE_PATH="$root/stdlib"
 
+$binary "$examples/c_file.ic" >/dev/null && echo c_file OK
 $binary "$examples/enum.ic" >/dev/null && echo enum OK
 $binary "$examples/factorial.ic" >/dev/null && echo factorial OK
 $binary "$examples/fibonacci.ic" >/dev/null && echo fibonacci OK
@@ -16,6 +17,6 @@ $binary "$examples/file.ic" >/dev/null && echo file OK
 $binary "$examples/fizzbuzz.ic" >/dev/null && echo fizzbuzz OK
 $binary "$examples/function_calls.ic" >/dev/null && echo function_calls OK
 $binary "$examples/mandelbrot.ic" >/dev/null && echo mandelbrot OK
-# $binary "$examples/ping_pong.ic" | head >/dev/null && echo ping_pong OK
+$binary "$examples/ping_pong.ic" >/dev/null && echo ping_pong OK
 $binary "$examples/primes.ic" >/dev/null && echo primes OK
 $binary "$examples/string_demo.ic" >/dev/null && echo string_demo OK
