156820f9b084b7b2047ec1a64c9ad6fca5663f37
asoffer <andy.soffer@gmail.com>
2023-10-29 21:46:09 -0400

    Deleting legacy code.

--- legacy/core/type_system/pointer.h
+++
@@ -1,6 +0,0 @@
-#ifndef ICARUS_CORE_TYPE_SYSTEM_POINTER_H
-#define ICARUS_CORE_TYPE_SYSTEM_POINTER_H
-
-#include "core/type_system/type_system.h"
-
-#endif  // ICARUS_CORE_TYPE_SYSTEM_POINTER_H
--- legacy/core/type_system/sized_integer.h
+++
@@ -1,8 +0,0 @@
-#ifndef ICARUS_CORE_TYPE_SYSTEM_SIZED_INTEGER_H
-#define ICARUS_CORE_TYPE_SYSTEM_SIZED_INTEGER_H
-
-#include "core/alignment.h"
-#include "core/bytes.h"
-#include "core/type_system/type_system.h"
-
-#endif  // ICARUS_CORE_TYPE_SYSTEM_SIZED_INTEGER_H
